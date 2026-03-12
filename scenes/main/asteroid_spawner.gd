d6b022cee6203a1dc54345c6b3b4b307c1cc7e29
Carlos Vargas <cavu93@hotmail.com>
2026-03-11 22:29:48 -0600

    feat(game):modified Se modifica metodo qncargado de generar los asteroidesd Changes to be committed: 	modified:   scenes/main/asteroid_spawner.gd

--- scenes/main/asteroid_spawner.gd
+++ scenes/main/asteroid_spawner.gd
@@ -7,6 +7,5 @@ func create_asteroid():
 	var big_asteroid_instance = big_asteroid_scene.instantiate() # al llamarlo creo una instancia de la escena en memoria
 	add_child(big_asteroid_instance) #agregarlo al arbol de nodos
 
-
 func _on_timer_timeout() -> void:
 	create_asteroid() # Replace with function body.
