7a1755a4398f687a455632e941dabdf8f1433856
Carlos Vargas <cavu93@hotmail.com>
2026-04-02 22:24:11 -0600

    feat(main): add Se agrega velocidad rusabndo randi  Changes to be committed: 	modified:   scenes/main/main.tscn

--- scenes/main/asteroid_spawner.gd
+++ scenes/main/asteroid_spawner.gd
@@ -2,10 +2,14 @@ extends Marker2D
 
 @export var big_asteroid_scene:PackedScene # tipo de variable es PackedScene y es el archivo donde esta la escena 
 
+@export var min_x : float 
+@export var min_y : float 
 
 func create_asteroid():
 	var big_asteroid_instance = big_asteroid_scene.instantiate() # al llamarlo creo una instancia de la escena en memoria
 	add_child(big_asteroid_instance) #agregarlo al arbol de nodos
-
+	var random_y = randf_range(min_x,min_y)
+	big_asteroid_instance.global_position.y = random_y
+	  
 func _on_timer_timeout() -> void:
 	create_asteroid() # Replace with function body.
