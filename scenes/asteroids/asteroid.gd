fa59636bac70be7821020f49b15ed6e9bf06d194
Carlos Vargas <cavu93@hotmail.com>
2026-04-02 23:08:37 -0600

    feat(UI):add se agrega funcionalidad rand de rotacion  Changes to be committed: 	modified:   scenes/asteroids/asteroid.gd 	modified:   scenes/asteroids/big_asteroid.tscn

--- scenes/asteroids/asteroid.gd
+++ scenes/asteroids/asteroid.gd
@@ -1,7 +1,19 @@
 extends Area2D
 
-@export var speed : float
+@export var min_speed : float = 100
+@export var max_speed : float = 300
+@export var max_rotation_speed : float = 180
+@export var min_rotation_speed : float = -180
 
+var random_speed
+var random_rotation_speed
+
+
+func _ready() -> void:
+	random_speed = randf_range(min_speed,max_speed)
+	random_rotation_speed  = randf_range(min_rotation_speed,max_rotation_speed)
+	
 
 func _process(delta: float) -> void:
-	position.x -= speed * delta  # se resta porque quiero el movimiento de derecha a izquierda
+	position.x -= random_speed * delta  # se resta porque quiero el movimiento de derecha a izquierda
+	rotation_degrees += random_rotation_speed * delta
