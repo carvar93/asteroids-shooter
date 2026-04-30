56dd43aeb9a618a6f61c0fecc69b3a59e75111f2
Carlos Vargas <cavu93@hotmail.com>
2026-04-29 20:57:12 -0600

    feat(Laser) add Se agrega instancia del laser Changes to be committed: 	modified:   project.godot 	modified:   scenes/laser/laser.tscn 	modified:   scenes/main/main.tscn 	modified:   scenes/player/player.gd 	modified:   scenes/player/player.tscn 	new file:   scenes/player/player.tscn1803018595.tmp

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,14 +1,27 @@
 extends CharacterBody2D
 
+@export var laser_scene:PackedScene
 @export var speed :float
 
-func _physics_process(delta: float) -> void:
-	#Fisicas
-	#Movimiento de cuerpos fisicos (characterBody2D,RigidBody2d)
-	var y_input = Input.get_axis("up","down") # las 2 inputs definidas 
-	velocity.y = y_input * speed
+
 	
-	var x_input = Input.get_axis("left","right")
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("shoot"):
+		create_laser()
+
+	process_inputs()
+	move_and_slide()
+
+
+func process_inputs():
+	var y_input = Input.get_axis("up", "down")
+	velocity.y = y_input * speed
+
+	var x_input = Input.get_axis("left", "right")
 	velocity.x = x_input * speed
 	
-	move_and_slide()
+
+func create_laser():
+	var laser_instance = laser_scene.instantiate()
+	add_sibling(laser_instance)
+	laser_instance.global_position = $ShootPoint.global_position
