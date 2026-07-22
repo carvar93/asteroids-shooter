4b481997dd8c974a7c92f0af666bbefdb652d8b4
Carlos Vargas <cavu93@hotmail.com>
2026-07-21 20:43:32 -0600

    fix: Se corrige limite del la escena main
    Changes to be committed:
    	modified:   laser.gd
    	modified:   scenes/laser/laser.tscn
    	modified:   scenes/main/main.tscn

--- laser.gd
+++ laser.gd
@@ -16,3 +16,7 @@ func _process(delta: float) -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("asteroid"):
 		queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
